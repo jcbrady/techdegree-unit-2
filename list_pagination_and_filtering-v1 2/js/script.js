529d05ae9625d9bf7fafb6ac9024207f938f736e
JC <jcbrady89@yahoo.com>
2020-08-20 12:21:07 -0600

    Update script.js
    Working more with the second function. appendPageLinks()

--- list_pagination_and_filtering-v1 2/js/script.js
+++ list_pagination_and_filtering-v1 2/js/script.js
@@ -45,30 +45,31 @@ const showPage = (list, page) => {
   let startIndex = page * numberPerPage - numberPerPage
   let endIndex = page * numberPerPage
   let ul = document.getElementsByClassName("student-list")[0]
-  console.log("Start and end Index:")
-  console.log(startIndex)
-  console.log(endIndex)
+  //console.log("Start and end Index:")
+  //console.log(startIndex)
+  //console.log(endIndex)
 
   // hide list items
   for (let i = 0; i < list.length; i++) {
     list[i].style.display = "none"
-    console.log("hide all")
-    console.log(list[i])
+    //console.log("hide all")
+    //console.log(list[i])
 
     // show list items within the range
     if (i >= startIndex && i < endIndex) {
       list[i].style.display = ""
-      console.log("... but show this one:")
-      console.log(list[i])
+      //console.log("... but show this one:")
+      //console.log(list[i])
       //ul.appendChild(list)
     }
   }
   return list
 }
-console.log("line before showPage is called ...")
-showPage(studentList, 1)
+//console.log("line before showPage is called ...")
+//showPage(studentList, 1)
 //console.log("call showPage:")
 //console.log(showPage(studentList, 1))
+
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
@@ -77,32 +78,37 @@ showPage(studentList, 1)
 // PAGINATION
 
 function appendPageLinks(list) {
-  const pageTotal = list.length / numberPerPage
+  let pageTotal = list.length / numberPerPage
+  pageTotal = Math.ceil(pageTotal)
   let pageCounter = "1"
+  let tList = ""
   const pageDiv = document.getElementsByClassName("page")[0]
 
-  // console.log("kazooz!")
-  // console.log(list)
-  // console.log(pageTotal)
-
-  // create elements
+  // create pagination elements
   const pDiv = document.createElement("div")
   const ul = document.createElement("ul")
   //const li = document.createElement("li")
   //const a = document.createElement("a")
 
-  // structure and add elements to DOM
+  // structure and add pagination elements to DOM
   pDiv.className = "pagination"
   pageDiv.appendChild(pDiv)
   pDiv.appendChild(ul)
 
+  // loop through the list show only the display results
+  for (i = 0; i < list.length; i++) {
+    if (list[i].style.display == "") {
+      tList = list[i]
+    }
+  }
+  // loop through the pageTotal variable to create an li link for each page
   for (i = 1; i <= pageTotal; i++) {
     const li = document.createElement("li")
     const a = document.createElement("a")
     //let func = showPage(studentList, [i])
     //console.log(func)
 
-    a.href = "#" // link must be to call this function??? showPage(studentList, [i])
+    a.href = showPage(tList, [i]) // link must be to call this function??? showPage(studentList, [i])
     a.textContent = pageCounter++
     a.className = ""
     ul.appendChild(li)
